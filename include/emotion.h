af724cd833e561707b95efbdab487277262f638d
trungnguyen278 <trung2782002@gmail.com>
2025-11-07 15:55:26 +0700

    update list animation for free roaming

--- include/emotion.h
+++ include/emotion.h
@@ -8,13 +8,21 @@
 #include "..\resource\emoji_h\buon.h"
 #include "..\resource\emoji_h\hoamat.h"
 
+#include "..\resource\emoji_h\buonngu.h"
+#include "..\resource\emoji_h\nhaymat.h"
+#include "..\resource\emoji_h\suynghi2.h"
+#include "..\resource\emoji_h\ngacnhien.h"
+
 #define EMOTION_NEUTRAL      0       // Bình thường
 #define EMOTION_HAPPY        1       // Vui vẻ
 #define EMOTION_SAD          2       // Buồn
 #define EMOTION_STUNNED      3       // Hóa đá
 
-VideoInfo* videoList[] = { &binhthuong, &vuive, &buon, &hoamat };
+// List of emotions
+VideoInfo* emotionList[] = { &binhthuong, &vuive, &buon, &hoamat };
 
+// List of animations for free roaming
+VideoInfo* animationList[] = { &buonngu, &nhaymat, &suynghi2, &ngacnhien };
 
 
 #endif // EMOTION_H
